eade65e7322b6de6040bf9fbd1f12906232a4e09
esperr <ed_sperr@hotmail.com>
2021-09-29 15:37:34 -0400

    Cull the Legacy PubMed bits
    Legacy PubMed is dead -- long live New PubMed (which is just regular PubMed now...)

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import json
 from bs4 import BeautifulSoup
 import urllib
 import re
-#from operator import itemgetter
 from google.appengine.api import urlfetch
 from google.appengine.ext import ndb
 import time
@@ -15,70 +14,9 @@ import webapp2
 
 localtime = time.asctime( time.localtime(time.time()) )
 
-def getBlobid(searchstr):
-    urlfetch.set_default_fetch_deadline(60)
-    #first we call PubMed and scrape the blobid off the page
-    urlstem = "https://www.ncbi.nlm.nih.gov/pubmed/?term="
-    url = urlstem + searchstr
-    try:
-        validate_certificate = 'true'
-        result = urlfetch.fetch(url)
-        if result.status_code == 200:
-            myText = result.content
-            m = re.search('blobid=(.*):yr=', myText)
-            if m:
-                blobId = m.group(1)
-                #newResponse = fetchCsv(blobId, searchstr)
-                return blobId
-            else:
-                return "Too few results!"
-        else:
-            return result.status_code
-    except urlfetch.Error:
-        logging.exception('Caught exception fetching url')
-
-
-def fetchCsv(blobId, searchstr):
-    urlfetch.set_default_fetch_deadline(60)
-    #now we take that blobid and request the CSV
-    csvstem = "https://www.ncbi.nlm.nih.gov/pubmed?p$l=Email&Mode=download&dlid=timeline&filename=timeline.csv"
-    try:
-        validate_certificate = 'true'
-        csvurl = csvstem + "&term=" + searchstr + "&bbid=" + blobId + "&p$debugoutput=off"
-        result = urlfetch.fetch(csvurl)
-        if result.status_code == 200:
-            yearCounts = {
-                "timestamp": localtime,
-                "search": searchstr,
-                "counts": {}
-            }
-            myText = (result.content).splitlines()
-            myText.pop(0)
-            myText.pop(0)
-            myText.pop()
-            for l in myText:
-                l = l.split(',')
-                year = l.pop(0)
-                count = l.pop(0)
-                yearCounts['counts'][year] = count
-                    #yearcount = {
-                    #    "year": year,
-                    #    "count": count,
-                    #}
-                    #yearCounts['counts'].append(yearcount)
-
-        else:
-            return result.status_code
-    except urlfetch.Error:
-            logging.exception('Caught exception fetching url')
-
-        #yearCounts['counts'] = sorted(yearCounts['counts'], key=itemgetter('year'))
-    prettiness = json.dumps(yearCounts, sort_keys=True, indent=4)
-    return prettiness
 
 def fetchNewCounts(searchstr):
     urlfetch.set_default_fetch_deadline(60)
-    #now we take that blobid and request the CSV
     labstem = "https://pubmed.ncbi.nlm.nih.gov/?term="
     url = labstem + searchstr
     try:
@@ -117,20 +55,6 @@ class myJson(ndb.Model):
     applicationName = ndb.StringProperty()
     json = ndb.TextProperty()
 
-class getCounts(webapp2.RequestHandler):
-    def get(self):
-        query = self.request.get('q')
-        searchstr = urllib.quote_plus(query)
-        myBlobid = getBlobid(searchstr)
-        logging.info(myBlobid)
-        if myBlobid == "Too few results!":
-            thisJson = json.dumps({ 'error': 'None or too few results' })
-        else:
-            thisJson = fetchCsv(myBlobid, searchstr)
-        self.response.headers = { "Content-Type": "application/json; charset=UTF-8",
-         "Access-Control-Allow-Origin": "*" }
-        self.response.write(thisJson)
-
 class getNewCounts(webapp2.RequestHandler):
     def get(self):
         self.response.headers = { "Content-Type": "application/json; charset=UTF-8",
@@ -139,23 +63,6 @@ class getNewCounts(webapp2.RequestHandler):
         searchstr = urllib.quote_plus(query)
         myResponse = fetchNewCounts(searchstr)
         self.response.write(myResponse)
-
-class BuildBaseCounts(webapp2.RequestHandler):
-    def get(self):
-        searchstr = "all[sb]"
-        myBlobid = getBlobid(searchstr)
-        print myBlobid
-        thisJson = fetchCsv(myBlobid, searchstr)
-        self.response.write(thisJson)
-
-        gotJson = myJson.query(myJson.applicationName=='medtime').fetch()
-        if len(gotJson) > 0:
-            baseline = gotJson[0]
-            baseline.json = thisJson
-            baseline.put()
-        else:
-            baselinejson = myJson(applicationName='medtime', json=thisJson)
-            baselinejson.put()
 
 class BuildNewBaseCounts(webapp2.RequestHandler):
     def get(self):
@@ -180,9 +87,7 @@ class ShowBaseCounts(webapp2.RequestHandler):
         self.response.write(latestJson.json)
 
 app = webapp2.WSGIApplication([
-    ('/search', getCounts),
     ('/newsearch', getNewCounts),
-    ('/buildbasecounts', BuildBaseCounts),
     ('/buildnewbasecounts', BuildNewBaseCounts),
     ('/showbasecounts', ShowBaseCounts),
 ], debug=True)
